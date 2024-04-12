1a3bd34b5dec11f3a37f773ea8cfa09e8a6f23ce
ZorEl212 <yabsirad212@proton.me>
2024-04-12 03:32:06 +0300

    Fix shebang and pep-8
    Signed-off-by: ZorEl212 <yabsirad212@proton.me>

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,10 @@
+#!/usr/bin/python3
+"""Pascal's Triangle"""
+
+
 def pascal_triangle(n):
+    """Returns a list of lists of integers representing the -
+    Pascal's triangle"""
     if n <= 0:
         return []
 
